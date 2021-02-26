701b25579a9c61a6d7a23367d8485438aaf8a109
i-cluster <jhlee9091@gmail.com>
2021-02-26 19:37:01 +0900

    210225_러시아 국기

--- 0225_러시아 국기.py
+++ 0225_러시아 국기.py
@@ -6,12 +6,18 @@ sys.stdin = open('sample.txt', 'r')
 
 for tc in range(1, int(input())+1):
     n, m = map(int, input().split())
-    words = []
+    color = []
 
     for _ in range(n):
         arr = input()
-        words.append(tuple(map(lambda x: m - x, (arr.count('W'), arr.count('B'), arr.count('R')))))
+        color.append(list(map(lambda x: m - x, (arr.count('W'), arr.count('B'), arr.count('R')))))
 
-    
+    paint = list(zip(*color))
 
-    print(f'#{tc} {words}')
+    min_paint = n * m
+    for i in range(1, n-1):
+        for j in range(i+1, n):
+            s = sum(paint[0][:i]) + sum(paint[1][i:j]) + sum(paint[2][j:])
+            if s < min_paint: min_paint = s
+
+    print(f'#{tc} {min_paint}')
