22ec4045d348a02858c6a019882c16759fabd74d
i-cluster <jhlee9091@gmail.com>
2021-03-21 00:22:30 +0900

    210320_보물상지 비밀번호

--- 0320_보물상자 비밀번호.py
+++ 0320_보물상자 비밀번호.py
@@ -6,17 +6,14 @@ dic = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
 import sys
 sys.stdin = open('sample.txt', 'r')
 
-for tc in range(int(input())+1):
+for tc in range(1, int(input())+1):
     n, k = map(int, input().split())
     hex_str = input() * 2
 
-    l, nums = len(hex_str)//8, []
+    l, nums = n//4, []
     for i in range(l):
         for p in range(4):
-            s = hex_str[i+(p*l):i+(p+1)*l]
-            pw = 0
-            for j in range(l):
-                pw += s[j] * (16 ** j)
+            pw = hex_str[p * l + i:(p + 1) * l + i]
             if pw not in nums: nums.append(pw)
 
-    print(f'#{tc} {sorted(nums)[k]}')
+    print(f'#{tc} {int("0x" + sorted(nums, reverse=True)[k-1], 16)}')
